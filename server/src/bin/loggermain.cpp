6fedbb11c52c9912a312dcd9e7d4128c3014c2e6
Alexander Kolli <ppi@magnificat.at>
2009-08-19 17:43:11 +0000

    add second binary to project now only to testing cmake in eclipse

---
+++ server/src/bin/loggermain.cpp
@@ -0,0 +1,33 @@
+/**
+ *   This file is part of ppi-server.
+ *
+ *   ppi-server is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ppi-server is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with ppi-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//#include <string.h>
+//#include <stdlib.h>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// this file is now only for tesing cmake with eclipse
+	// to create an second binary
+	cout << "start ppi-log-server" << endl;
+	return 0;
+}
