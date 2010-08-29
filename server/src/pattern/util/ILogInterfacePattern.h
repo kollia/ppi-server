6b81b7c9840c1f2088205c7c1d8e8d12678504f9
Alexander Kolli <ppi@magnificat.at>
2010-08-29 12:31:42 +0000

    create new pattern for logging service and also for LogHolderPattern need's an body -> create CMakeList.txt

---
+++ server/src/pattern/util/ILogInterfacePattern.h
@@ -0,0 +1,102 @@
+/**
+ *   This file is part of ppi-server.
+ *
+ *   ppi-server is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ppi-server is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with ppi-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ILOGINTERFACEPATTERN_H_
+#define ILOGINTERFACEPATTERN_H_
+
+
+#include <string>
+
+using namespace std;
+
+namespace design_pattern_world
+{
+	namespace util_pattern
+	{
+	/**
+	 * pattern class for an logging object
+	 *
+	 * @autor Alexander Kolli
+	 * @version 1.0.0
+	 */
+		class ILogInterfacePattern
+		{
+		public:
+			/**
+			 * write vector log and threadNames which was read
+			 * before get connection
+			 *
+			 * @return whether write any message over connection
+			 */
+			virtual bool writeVectors()= 0;
+			/**
+			 * open the connection to server for sending questions
+			 * <b>errorcodes:</b>
+			 * <table>
+			 * 	<tr>
+			 * 		<td>
+			 * 			0
+			 * 		</td>
+			 * 		<td>
+			 * 			no error occurred
+			 * 		</td>
+			 * 	</tr>
+			 * 	<tr>
+			 * 		<td>
+			 * 			-1
+			 * 		</td>
+			 * 		<td>
+			 * 			WARNING: connection exist before
+			 * 		</td>
+			 * 	</tr>
+			 * 	<tr>
+			 * 		<td>
+			 * 			1
+			 * 		</td>
+			 * 		<td>
+			 * 			ERROR: no <code>IClientConnectArtPattern</code> be given for sending
+			 * 		</td>
+			 * 	</tr>
+			 * 	<tr>
+			 * 		<td>
+			 * 			2
+			 * 		</td>
+			 * 		<td>
+			 * 			cannot connect with server, or initialization was fail
+			 * 		</td>
+			 * 	</tr>
+			 * 	<tr>
+			 * 		<td colspan="2">
+			 * 			all other ERRORs or WARNINGs see in <code>IClientConnectArtPattern</code>
+			 * 			for beginning connection by sending
+			 * 		</td>
+			 * 	</tr>
+			 * </table>
+			 *
+			 * @param toopen string for open question, otherwise by null the connection will be open with '<process>:<client> SEND' for connect with an ServerMethodTransaction
+			 * @return error number
+			 */
+			virtual int openSendConnection(string toopen= "")= 0;
+			/**
+			 * this method will be called when ConnectionChecker ending
+			 */
+			virtual void connectionCheckerStops()= 0;
+		};
+	}
+}
+
+#endif /* ILOGINTERFACEPATTERN_H_ */
---
+++ server/src/pattern/util/LogHolderPattern.h
@@ -0,0 +1,119 @@
+/**
+ *   This file is part of ppi-server.
+ *
+ *   ppi-server is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ppi-server is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with ppi-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LOGHOLDERPATTERN_H_
+#define LOGHOLDERPATTERN_H_
+
+#include <iostream>
+#include <string>
+
+#include "ILogPattern.h"
+
+using namespace std;
+using namespace design_pattern_world::util_pattern;
+
+class LogHolderPattern
+{
+public:
+	/**
+	 * initialization of object.<br />
+	 * This object do not delete instance of giving logging process.
+	 *
+	 * @param loggingobject object to logging messages
+	 */
+	static void init(ILogPattern* loggingobject)
+	{ if(LogHolderPattern::_instance) delete LogHolderPattern::_instance;
+	  LogHolderPattern::_instance= new LogHolderPattern;
+	  LogHolderPattern::_instance->m_oLogging= loggingobject; LogHolderPattern::_instance->m_bExists= true;
+	  loggingobject->callback(LogHolderPattern::usable);				};
+	/**
+	 * get actual instance of object
+	 */
+	static LogHolderPattern* instance()
+	{ if(!_instance) _instance= new LogHolderPattern; return _instance; };
+	/**
+	 * callback routine to get information when logging process destroy
+	 */
+	static void* usable(bool use)
+	{ _instance->m_bExists= use; };
+	/**
+	 * set for all process or thread an name to identify in log-messages
+	 *
+	 * @param name specified name
+	 */
+	void setThreadName(string threadName)
+	{ if(m_bExists) m_oLogging->setThreadName(threadName); };
+	/**
+	 * return name of thread from given thread-id.<br />
+	 * If no logging process is given, it returning the string that no logging process exists.
+	 *
+	 * @param threadID id of thread
+	 */
+	string getThreadName(pthread_t threadID= 0) const
+	{ if(m_bExists) return m_oLogging->getThreadName(threadID); else return "no logging process exists"; };
+	/**
+	 * to log an message
+	 *
+	 * @param file name from witch source file the method is called, specified with <code>__FILE__</code>
+	 * @param line number of line in the source file, specified with <code>__LINE__</code>
+	 * @param type defined type of log-message (<code>LOG_DEBUG, LOG_INFO, ...</code>)
+	 * @param message string witch should written into log-files
+	 * @param sTimeLogIdentif if this identifier be set, the message will be write only in an defined time
+	 */
+	void log(string file, int line, int type, string message, string sTimeLogIdentif= "")
+	{ 	if(m_bExists) m_oLogging->log(file, line, type, message, sTimeLogIdentif); else
+		{	cout << "****************************************************************************" << endl;
+			cout << file << " LINE: " << line << endl << message << endl; 									}	};
+	/**
+	 * destructor of object
+	 */
+	virtual ~LogHolderPattern()
+	{ _instance= NULL; };
+
+
+private:
+	/**
+	 * whether object of logging exists
+	 */
+	bool m_bExists;
+	/**
+	 * instance of object
+	 */
+	static LogHolderPattern* _instance;
+	/**
+	 * object to logging messages
+	 */
+	ILogPattern* m_oLogging;
+
+	/**
+	 * Constructor of singelton pattern
+	 */
+	LogHolderPattern() :
+		m_bExists(false),
+		m_oLogging(NULL) {};
+
+};
+
+
+#ifndef LOG
+#define LOG(type, message) LogHolderPattern::instance()->log(__FILE__, __LINE__, type, message)
+#endif // LOG
+#ifndef TIMELOG
+#define TIMELOG(type, identif, message) LogHolderPattern::instance()->log(__FILE__, __LINE__, type, message, identif)
+#endif // TIMELOG
+
+#endif /* LOGHOLDERPATTERN_H_ */
