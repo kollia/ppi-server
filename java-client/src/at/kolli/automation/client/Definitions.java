47f0a140f3c842ceaef94f0088a31e0d682cc444
Alexander Kolli <ppi@magnificat.at>
2011-07-20 16:54:59 +0000

    global class to set client VERSIONS and PROTOCOL

---
+++ java-client/src/at/kolli/automation/client/Definitions.java
@@ -0,0 +1,12 @@
+package at.kolli.automation.client;
+
+public class Definitions 
+{
+	static final int MAJOR_RELEASE= 0;
+	static final int MINOR_RELEASE= 2;
+	static final int SUBVERSION= 0;
+	static final int PATCH_LEVEL= 0;
+	static final int REVISION_NUMBER= 505;
+	
+	static float SERVER_PROTOCOL= 1.0F;
+}
