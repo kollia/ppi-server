bcc460d627900a7c3838f16d34b8d4f2636c04cd
Alexander Kolli <ppi@magnificat.at>
2011-11-14 17:19:27 +0000

    shift the interface of owreader from portserver/lib into portserver/libs/interface

---
+++ server/src/portserver/libs/interface/OWInterface.cpp
@@ -0,0 +1,354 @@
+/**
+ *   This file is part of ppi-server.
+ *
+ *   ppi-server is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ppi-server is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with ppi-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../../../pattern/util/LogHolderPattern.h"
+
+#include "../../../util/stream/IParameterStringStream.h"
+
+#include "OWInterface.h"
+
+namespace server {
+
+map<unsigned short, SHAREDPTR::shared_ptr<OWInterface> > OWInterface::_instances;
+
+inline SHAREDPTR::shared_ptr<OWInterface> OWInterface::getServer(const unsigned short serverID)
+{
+	return getServer("", NULL, serverID);
+}
+
+SHAREDPTR::shared_ptr<OWInterface> OWInterface::getServer(const string& process, IClientConnectArtPattern* connection, const unsigned short serverID)
+{
+	int err;
+	ostringstream sID;
+	map<unsigned short, SHAREDPTR::shared_ptr<OWInterface> >::iterator it;
+
+	it= _instances.find(serverID);
+	if(it != _instances.end())
+	{
+		if(connection)
+			delete connection;
+		return it->second;
+	}
+	sID << serverID;
+	if(!connection)
+	{
+		string msg;
+
+		msg=  "### ERROR: no instance for ID ";
+		msg+= sID.str() + " exist! Create first with getServer(processName, connection, serverID).";
+		cerr << msg << endl;
+		LOG(LOG_ERROR, msg);
+		return SHAREDPTR::shared_ptr<OWInterface>();
+	}
+	_instances[serverID]= SHAREDPTR::shared_ptr<OWInterface>(new OWInterface(process, sID.str(), connection));
+	err= _instances[serverID]->openSendConnection();
+	if(err > 0)
+	{
+		string msg;
+		msg=  "### ERROR: cannot open connection to one wire server " + sID.str() + "\n";
+		msg+= "           " + _instances[serverID]->strerror(err);
+		cerr << msg << endl;
+		LOG(LOG_ERROR, msg);
+		_instances.erase(serverID);
+		return SHAREDPTR::shared_ptr<OWInterface>();
+	}
+	return _instances[serverID];
+}
+
+SHAREDPTR::shared_ptr<OWInterface> OWInterface::getServer(const string& type, const string& chipID)
+{
+	for(map<unsigned short, SHAREDPTR::shared_ptr<OWInterface> >::iterator it= _instances.begin(); it != _instances.end(); ++it)
+	{
+		if(it->second->isServer(type, chipID))
+			return it->second;
+	}
+	return SHAREDPTR::shared_ptr<OWInterface>();
+}
+
+string OWInterface::getServerName()
+{
+	string sRv;
+	OMethodStringStream method("getServerName");
+
+	sRv= sendMethod(m_stoClient, method, true);
+	return sRv;
+}
+
+void OWInterface::endOfInitialisation(const int maxServer, bool out)
+{
+	OWI pFirst;
+	OMethodStringStream method("endOfInitialisation");
+
+	if(_instances.size())
+	{
+		method << maxServer;
+		method << out;
+		pFirst= _instances.begin()->second;
+		pFirst->sendMethod("ppi-db-server", method, false);
+	}
+}
+
+bool OWInterface::isServer(const string& type, const string& chipID)
+{
+	string res;
+	OMethodStringStream method("isServer");
+
+	method << type;
+	method << chipID;
+	res= sendMethod(m_stoClient, method, true);
+	if(res == "true")
+		return true;
+	return false;
+}
+
+string OWInterface::getChipType(const string& ID)
+{
+	string sRv;
+	OMethodStringStream method("getChipType");
+
+	method << ID;
+	sRv= sendMethod(m_stoClient, method, true);
+	return sRv;
+}
+
+string OWInterface::getChipFamily(const string& ID)
+{
+	string sRv;
+	OMethodStringStream method("getChipFamily");
+
+	method << ID;
+	sRv= sendMethod(m_stoClient, method, true);
+	return sRv;
+}
+
+vector<string> OWInterface::getChipIDs()
+{
+	vector<string> vRv;
+	OMethodStringStream method("getChipIDs");
+
+	vRv= sendMethod(m_stoClient, method, "done", true);
+	return vRv;
+}
+
+vector<string> OWInterface::getDebugInfo()
+{
+	vector<string> vRv;
+	OMethodStringStream method("getDebugInfo");
+
+	vRv= sendMethod(m_stoClient, method, "done", true);
+	return vRv;
+}
+
+void OWInterface::setDebug(bool set)
+{
+	OMethodStringStream method("setDebug");
+
+	method << set;
+	sendMethod(m_stoClient, method, false);
+}
+
+bool OWInterface::write(const string& id, const double value, const string& addinfo)
+{
+	string res;
+	OMethodStringStream method("write");
+
+	method << id;
+	method << value;
+	method << addinfo;
+	res= sendMethod(m_stoClient, method, true);
+	if(res == "true")
+		return true;
+	return false;
+}
+
+bool OWInterface::read(const string& id, double* value)
+{
+	bool bRv;
+	string res;
+	OMethodStringStream method("read");
+
+	method << id;
+	method << *value;
+	res= sendMethod(m_stoClient, method, true);
+
+	IParameterStringStream result(res);
+
+	result >> bRv;
+	result >> *value;
+	return bRv;
+}
+
+int OWInterface::command_exec(const bool wait, const string& command, vector<string>& result, bool& more)
+{
+	int nRv= -1;
+	string error;
+	vector<string>::size_type nSize;
+	OMethodStringStream method("command_exec");
+
+	method << command;
+	result= sendMethod(m_stoClient, method, "done", wait);
+	nSize= result.size();
+	if(	nSize == 1 &&
+		result[0] == "done"	&&
+		wait == false			)
+	{
+		result.clear();
+		more= false;
+		return 0;
+
+	}
+	if(	nSize > 0 &&
+		result[nSize-1] == "done"	)
+	{
+		result.pop_back();
+		--nSize;
+	}
+	if(	nSize == 1 &&
+		(	result[nSize-1].substr(0, 7) == "WARNING" ||
+			result[nSize-1].substr(0, 5) == "ERROR"		)	)
+	{
+		istringstream res(result[nSize-1]);
+
+		res >> error;
+		res >> nRv;
+		if(error == "ERROR")
+			nRv+= -1;
+		more= false;
+		return nRv;
+
+	}else if(nSize < 2)
+	{
+		string err;
+
+		err= "getting undefined result from SHELL reader " + m_stoClient + "\n";
+		err+= "by sending command '" + method.str() + "\n";
+		err+= "--------\n";
+		for(vector<string>::iterator o= result.begin(); o != result.end(); ++o)
+			err+= *o + "\n";
+		err+= "--------\n";
+		LOG(LOG_ERROR, err);
+		result.clear();
+		more= false;
+		return -1;
+	}
+
+	istringstream morecontent(result[nSize-2]);
+	istringstream errorlevel(result[nSize-1]);
+
+	morecontent >> error;// not used as error, variable should be MORECONTENT
+	if(error == "MORECONTENT")
+	{
+		morecontent >> boolalpha >> more;
+		result.pop_back();
+	}
+	errorlevel >> error;// not used as error, variable should be ERRORLEVEL
+	if(error == "ERRORLEVEL")
+	{
+		errorlevel >> nRv;
+		result.pop_back();
+	}
+	return nRv;
+}
+
+void OWInterface::range(const string& pin, double& min, double& max, bool& bfloat)
+{
+	string res;
+	OMethodStringStream method("range");
+
+	method << pin;
+	res= sendMethod(m_stoClient, method, true);
+
+	IParameterStringStream result(res);
+
+	result >> min;
+	result >> max;
+	result >> bfloat;
+}
+
+bool OWInterface::haveToBeRegistered()
+{
+	string res;
+	OMethodStringStream method("haveToBeRegistered");
+
+	res= sendMethod(m_stoClient, method, true);
+	if(res == "true")
+		return true;
+	return false;
+}
+
+short OWInterface::useChip(IActionPropertyPattern* properties, string& unique, const string& folder, const string& subroutine)
+{
+	short nRv;
+	string res;
+	OMethodStringStream method("useChip");
+
+	method << properties->str();
+	method << unique;
+	method << folder;
+	method << subroutine;
+	res= sendMethod(m_stoClient, method, true);
+
+	IParameterStringStream result(res);
+
+	result >> nRv;
+	result >> res;
+	properties->pulled(res);
+	result >> unique;
+	return nRv;
+}
+
+void OWInterface::usePropActions(const IActionPropertyPattern* properties)
+{
+	string pulled;
+	OMethodStringStream method("usePropActions");
+
+	method << properties->str();
+	pulled= sendMethod(m_stoClient, method, /*wait*/true);
+	properties->pulled(pulled);
+}
+
+void OWInterface::checkUnused(const int maxServer)
+{
+	OWI pFirst;
+	OMethodStringStream method("checkUnused");
+
+	if(_instances.size())
+	{
+		method << maxServer;
+		pFirst= _instances.begin()->second;
+		pFirst->sendMethod("ppi-db-server", method, true);// wait for ending
+	}
+}
+
+bool OWInterface::reachAllChips()
+{
+	string res;
+	OMethodStringStream method("reachAllChips");
+
+	res= sendMethod(m_stoClient, method, true);
+	if(res == "true")
+		return true;
+	return false;
+}
+
+void OWInterface::deleteAll()
+{
+	for(map<unsigned short, OWI>::iterator it= _instances.begin(); it != _instances.end(); ++it)
+		it->second->closeSendConnection();
+	_instances.clear();
+}
+}
---
+++ server/src/portserver/libs/interface/OWInterface.h
@@ -0,0 +1,244 @@
+/**
+ *   This file is part of ppi-server.
+ *
+ *   ppi-server is free software: you can redistribute it and/or modify
+ *   it under the terms of the Lesser GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   ppi-server is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *   You should have received a copy of the Lesser GNU General Public License
+ *   along with ppi-server.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OWINTERFACE_H_
+#define OWINTERFACE_H_
+
+#include <vector>
+#include <string>
+
+#include "../../../server/libs/client/ProcessInterfaceTemplate.h"
+#include "../../../util/smart_ptr.h"
+
+#include "../../../pattern/server/IClientConnectArtPattern.h"
+#include "../../../pattern/util/iactionpropertypattern.h"
+
+namespace server {
+
+using namespace util;
+using namespace design_pattern_world;
+using namespace design_pattern_world::server_pattern;
+
+class OWInterface : public ProcessInterfaceTemplate {
+public:
+	/**
+	 * get instance for OWServer.<br />
+	 * Instnace have to be exist, otherwise create first the instance with <code>getServer(processName, connection, serverID)</code>.
+	 *
+	 * @param serverID id of OWSerever to want reach
+	 * @return correct and checkd instance
+	 */
+	static SHAREDPTR::shared_ptr<OWInterface> getServer(const unsigned short serverID);
+	/**
+	 * get instance of OWServer and create if not exist.<br/>
+	 * By cancel this OWInterface objects with deleteAll(), second parameter object will be also delete in parent class.
+	 *
+	 * @param process name of process in which the interface running
+	 * @param connection to which server this interface should connect to send messages
+	 * @param serverID id of OWSerever to want reach
+	 * @return correct and checkd instance
+	 */
+	static SHAREDPTR::shared_ptr<OWInterface> getServer(const string& process, IClientConnectArtPattern* connection, const unsigned short serverID);
+	/**
+	 * method returning the right server for an specific chip ID.<br />
+	 * All OWServer instaces be defined in an vector in the constructor
+	 *
+	 * @param type type of server (OWFS, Vk8055, ...)
+	 * @param chipID specific ID which the server should holded
+	 * @return the server instance
+	 */
+	static SHAREDPTR::shared_ptr<OWInterface> getServer(const string& type, const string& chipID);
+	/**
+	 * display identification name for OWServer
+	 *
+	 * @return name of server
+	 */
+	string getServerName();
+	/**
+	 * all initialication of subroutines
+	 * are done
+	 *
+	 * @param maxServer count of running one wire reader
+	 * @param out whether result should be shown on command line
+	 */
+	static void endOfInitialisation(const int maxServer, bool out);
+	/**
+	 * whether server has type and chipID
+	 *
+	 * @param type type of server (OWFS, Vk8055, ...)
+	 * @param chipID specific ID which the server should holded
+	 * @return whether server has ID
+	 */
+	bool isServer(const string& type, const string& chipID);
+	/**
+	 * function reading in one wire filesystem
+	 * from the chip with the ID as subdirectory
+	 * the type
+	 *
+	 * @param ID XX.XXXXXXXXXX id from the chip beginning with two cahracter famaly code
+	 * @return string of type from dallas semicontactor beginning with 'DS'
+	 */
+	string getChipType(const string &ID);
+	/**
+	 * function reading in one wire filesystem
+	 * from the chip with the ID as subdirectory
+	 * the family code
+	 *
+	 * @param ID XX.XXXXXXXXXX id from the chip beginning with two character family code
+	 * @return two Hexa character family code geted from chip
+	 */
+	string getChipFamily(const string& ID);
+	/**
+	 * returning all chip id's in an vector
+	 *
+	 * @return dallas chip id's
+	 */
+	vector<string> getChipIDs();
+	/**
+	 * set debug parameter forOWServer benchmark
+	 *
+	 * @param set whether debug parameter should be set
+	 */
+	void setDebug(bool set);
+	/**
+	 * debug info for defined OWServer for benchmark
+	 *
+	 * @return benchmark strings
+	 */
+	vector<string> getDebugInfo();
+	/**
+	 * select all properties and actions which are used in interface
+	 *
+	 * @param properties reading properties from the main configuration file
+	 */
+	void usePropActions(const IActionPropertyPattern* properties);
+	/**
+	 * write to chip direct if pin is set to uncached,<br />
+	 * or write into cache for writing later
+	 *
+	 * @param id unique ID of pin on chip
+	 * @param value Value which should be writing
+	 * @param addinfo additional info for chip
+	 * @return whether writing on the device was by the last one correct
+	 */
+	bool write(const string& id, const double value, const string& addinfo);
+	/**
+	 * read from chipdirect if pin is set to uncached,<br />
+	 * or read from cahe which be actualiced in defined time
+	 *
+	 * @param id ID of dallas conductor
+	 * @param value Value which should be writing
+	 * @return whether the reading on device was correct
+	 */
+	bool read(const string& id, double* value);
+	/**
+	 * primary command for chip access library.<br />
+	 * If command is an null terminated string, this method should give back
+	 * an result of all usable chip ID's.<br />
+	 * For all other usable commands, please read the documentation of the library
+	 *
+	 * @param wait whether calling routine want to wait for result
+	 * @param command specific command for library
+	 * @param result output result of command
+	 * @param more whether method has more result content for the next time
+	 * @return returning error level of command, or 0 when it was done right
+	 */
+	virtual int command_exec(const bool wait, const string& command, vector<string>& result, bool& more);
+	/**
+	 * set min and max parameter to the range which can be set for the pin.<br />
+	 * If the pin is set from 0 to 1 for writing, in the config file can be set begin while and end.
+	 * Otherwise the range is only for calibrate the max and min value if set from client outher range.
+	 *
+	 * @param pin the pin for whitch the range be asked
+	 * @param min the minimal value
+	 * @param max the maximal value
+	 * @param bfloat whether the values can be float variables
+	 */
+	void range(const string& pin, double& min, double& max, bool &bfloat);
+	/**
+	 * whether chips for owserver have an default configuration file
+	 * and have to be registered
+	 *
+	 * @return whether chip shoud be registered
+	 */
+	bool haveToBeRegistered();
+	/**
+	 * define dallas chip is used
+	 *
+	 * @param properties IActionProperyPattern from current subroutine
+	 * @param return value of unique chip ID
+	 * @param folder in which folder the chip is used
+	 * @param subroutine in which subroutine the chip is used
+	 * @return 0 if the pin is not correct, 1 if the pin is for reading, 2 for writing or 3 for unknown (reading/writing)
+	 */
+	short useChip(IActionPropertyPattern* properties, string& unique, const string& folder, const string& subroutine);
+	/**
+	 * check whether all exist id's are used
+	 *
+	 * @param maxServer count of running one wire reader
+	 */
+	static void checkUnused(const int maxServer);
+	/**
+	 * return true when an chip in measure.conf is not defined
+	 */
+	bool hasUnusedIDs();
+	/**
+	 * whether all reachable chips are defined for this interface,
+	 * or one before. No more server with this interface must be start.
+	 *
+	 * @return true if all used, otherwise false
+	 */
+	virtual bool reachAllChips();
+	/**
+	 * delete all created instances
+	 */
+	static void deleteAll();
+	/**
+	 * dummy destructor of OWInterface
+	 */
+	virtual ~OWInterface() {};
+
+protected:
+	/**
+	 * creating instance of OWInterface.<br/>
+	 * By cancel this OWInterface object, second parameter object will be also delete in parent class.
+	 *
+	 * @param process name of process in which the interface running
+	 * @param serverID id of OWServer to want reach
+	 * @param connection to which server this interface should connect to send messages
+	 */
+	OWInterface(const string& process, const string& serverID, IClientConnectArtPattern* connection)
+	:	ProcessInterfaceTemplate(process, "OWInterface", "OwServerQuestion-"+serverID, connection, NULL),
+		m_stoClient("OwServerQuestion-"+serverID)
+	{ };
+
+private:
+	/**
+	 * all instances of OWInterface for created to OWServer with ID
+	 */
+	static map<unsigned short, SHAREDPTR::shared_ptr<OWInterface> > _instances;
+	/**
+	 * name of server to which should send questions
+	 */
+	string m_stoClient;
+};
+
+typedef SHAREDPTR::shared_ptr<OWInterface> OWI;
+
+}
+
+#endif /* OWINTERFACE_H_ */
