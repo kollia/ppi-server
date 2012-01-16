3d806ca0a496fe5375a414684fd2cb7d54e050c8
Alexander Kolli <ppi@magnificat.at>
2012-01-16 16:26:19 +0000

    make correction iside adapter because asyncExec calls before also syncExec

--- java-client/src/at/kolli/dialogs/DisplayAdapter.java
+++ java-client/src/at/kolli/dialogs/DisplayAdapter.java
@@ -18,6 +18,8 @@ package at.kolli.dialogs;
 
 import org.eclipse.swt.widgets.Display;
 
+import at.kolli.layout.HtmTags;
+
 /**
  * adapter pattern for synchronize SWT syncExec and asyncExec
  * for debugging messages
@@ -34,38 +36,48 @@ public class DisplayAdapter {
 	}
 	public static void syncExec(Runnable runnable, String def)
 	{
-		/*if(HtmTags.debug)
+		Thread t= null;
+		
+		if(	HtmTags.debug &&
+			HtmTags.syncSWTExec	)
 		{
-			System.out.println("synchronize display for SWT by " + runnable);
+			t= Thread.currentThread();
+			System.out.println(t.getName()+" synchronize display for SWT by " + runnable);
 			if(!def.equals(""))
 				System.out.println("   by defined position of " + def);
-		}*/
+		}
 		Display.getDefault().syncExec(runnable);
-		/*if(HtmTags.debug)
+		if(	HtmTags.debug &&
+			HtmTags.syncSWTExec	)
 		{
-			System.out.println("has synchronized by " + runnable);
+			System.out.println(t.getName()+" has synchronized by " + runnable);
 			if(!def.equals(""))
 				System.out.println("   by defined position of " + def);
-		}*/
+		}
 	}
 	public static void asyncExec(Runnable runnable)
 	{
-		syncExec(runnable, "");
+		asyncExec(runnable, "");
 	}
 	public static void asyncExec(Runnable runnable, String def)
 	{
-		/*if(HtmTags.debug)
+		Thread t= null;
+		
+		if(	HtmTags.debug &&
+			HtmTags.syncSWTExec	)
 		{
-			System.out.println("set asynchron display for SWT by " + runnable);
+			t= Thread.currentThread();
+			System.out.println(t.getName()+" set asynchron display for SWT by " + runnable);
 			if(!def.equals(""))
 				System.out.println("   by defined position of " + def);
-		}*/
-		Display.getDefault().syncExec(runnable);
-		/*if(HtmTags.debug)
+		}
+		Display.getDefault().asyncExec(runnable);
+		if(	HtmTags.debug &&
+			HtmTags.syncSWTExec	)
 		{
-			System.out.println("has synchronized by " + runnable);
+			System.out.println(t.getName()+" has synchronized by " + runnable);
 			if(!def.equals(""))
 				System.out.println("   by defined position of " + def);
-		}*/
+		}
 	}
 }
