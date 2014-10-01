1ff6c53503c442fce5d65c022306cfd39066e60d
Alexander Kolli <ppi@magnificat.at>
2014-10-01 18:52:52 +0000

    implement for URL object to hold an hole URI to get different parts (protocol/host/path/file/query) and make also possible to encode or decode

--- server/src/util/URL.cpp
+++ server/src/util/URL.cpp
@@ -35,6 +35,7 @@
 
 #include "URL.h"
 
+#include "../database/logger/lib/logstructures.h"
 #include "../pattern/util/LogHolderPattern.h"
 //#include "../logger/lib/LogInterface.h"
 
@@ -42,6 +43,172 @@ using namespace boost;
 using namespace boost::algorithm;
 
 namespace util {
+
+	void URL::set(const string& src)
+	{
+		string::size_type nTypeLen;
+		string address;
+		vector<string> spl;
+
+		m_nPort= 0;
+		m_sHost= "";
+		m_sPath= "";
+		m_sFile= "";
+		m_vsQuerys.clear();
+		m_sAnchor= "";
+		split(spl, src, is_any_of(":"));
+		if(spl.empty())
+			return;
+		if(spl.size() > 1)
+		{
+			if(spl[0] == "file")
+			{
+				m_eType= file;
+				nTypeLen= 4;
+
+			}else if(spl[0] == "http")
+			{
+				m_eType= http;
+				nTypeLen= 4;
+
+			}else if(spl[0] == "https")
+			{
+				m_eType= https;
+				nTypeLen= 5;
+
+			}else if(spl[0] == "SOA")
+			{
+				m_eType= SOA;
+				nTypeLen= 3;
+
+			}else
+			{
+				m_eType= file;
+				nTypeLen= 0;
+				address= src;
+			}
+			if(src.substr(nTypeLen, 3) == "://")
+				address= src.substr(nTypeLen + 3);
+			else
+			{
+				address= src;
+				if(nTypeLen != 0)
+					m_eType= unknown;
+			}
+		}else
+		{
+			m_eType= file;
+			address= src;
+		}
+		if(	m_eType != file &&
+			m_eType != unknown	)
+		{
+			/*
+			 * by files is for linux
+			 * an question mark
+			 * allowed in the path.
+			 * In this case do not pass this block
+			 */
+			split(spl, address, is_any_of("?"));
+			if(!spl.empty())
+			{
+				address= spl[0];
+				if(spl.size() > 1)
+				{
+					string query;
+					vector<string>::iterator it;
+					string::size_type npos;
+
+					it= spl.begin();
+					++it;
+					query= *it;
+					++it;
+					while(it != spl.end())
+					{
+						query+= "?" + *it;
+						++it;
+					}
+
+					npos= query.find_last_of('#', query.length() -1 );
+					if(npos != string::npos)
+					{
+						m_sAnchor= query.substr(npos + 1);
+						query= query.substr(0, npos);
+					}
+
+					if(query != "")
+					{
+						split(m_vsQuerys, query, is_any_of("&"));
+						if(query.substr(0, 1) == "&")
+							m_vsQuerys.front()= "&" + m_vsQuerys.front();
+						if(query.substr(query.length() - 1, 1) == "&")
+							m_vsQuerys.back()+= "&";
+					}
+				}
+			}
+		}//if(m_eType != file)
+		split(spl, address, is_any_of("/"));
+		if(m_eType != file)
+		{
+			m_sHost= spl[0];
+			spl.erase(spl.begin());
+		}
+		if(!spl.empty())
+		{
+			m_sFile= spl[spl.size() - 1];
+			if(m_sFile.substr(0, m_sFile.length() - 1) == "/")
+				m_sFile= "";//only path be given
+		}
+		if(address.length() > (m_sHost.length() + m_sFile.length()))
+		{
+			address= address.substr(m_sHost.length());
+			m_sPath= address.substr(0, address.length() - m_sFile.length());
+		}
+		if(m_sHost != "")
+		{
+			bool bOK(true);
+			istringstream oport;
+
+			split(spl, m_sHost, is_any_of(":"));
+			if(spl.size() == 2)
+			{
+				oport.str(spl[1]);
+				oport >> m_nPort;
+				if(oport.fail())
+					bOK= false;
+				else
+					m_sHost= spl[0];
+
+			}if(spl.size() != 1)
+				bOK= false;
+			if(!bOK)
+			{
+				m_eType= unknown;
+				m_sPath= "";
+				m_sFile= "";
+				m_vsQuerys.clear();
+				m_sAnchor= "";
+
+			}else if(m_nPort == 0)
+			{
+				switch(m_eType)
+				{
+				case http:
+					m_nPort= 80;
+					break;
+				case https:
+					m_nPort= 443;
+					break;
+				case SOA:
+					m_nPort= 80;
+					break;
+				default:
+					m_nPort= 0;
+					break;
+				}
+			}
+		}
+	}
 
 	string URL::addPath(const string& first, string second, bool always/*= true*/)
 	{
@@ -147,5 +314,227 @@ namespace util {
 		if(pos == string::npos)
 			return "./";
 		return filestring.substr(0, pos + 1);
+	}
+
+	string URL::getProtocolSpec() const
+	{
+		string sRv;
+
+		switch(m_eType)
+		{
+		case file:
+			sRv= "file://";
+			break;
+		case http:
+			sRv= "http://";
+			break;
+		case https:
+			sRv= "https://";
+			break;
+		case SOA:
+			sRv= "http://";
+			break;
+		default:
+			sRv= "";
+			break;
+		}
+		return sRv;
+	}
+
+	string URL::getAddress() const
+	{
+		string sRv;
+
+		sRv= addPath(m_sHost, m_sPath);
+		sRv= addPath(sRv, m_sFile);
+		return sRv;
+	}
+
+	string URL::getQuery() const
+	{
+		string sRv;
+		vector<string>::const_iterator it;
+
+		if(!m_vsQuerys.empty())
+		{
+			it= m_vsQuerys.begin();
+			sRv= *it;
+			++it;
+			while(it != m_vsQuerys.end())
+			{
+				sRv+= "&" + *it;
+				++it;
+			}
+		}
+		if(m_sAnchor != "")
+			sRv+= "#" + m_sAnchor;
+		return sRv;
+	}
+
+	string URL::getAbsolutePathQuery() const
+	{
+		string sRv, query;
+
+		if(m_eType == file)
+			return "";
+		sRv= m_sPath;
+		query= getQuery();
+		if(query != "")
+			sRv+= "?" + query;
+		return sRv;
+	}
+
+	string URL::getSpecAddress() const
+	{
+		string sRv;
+
+		sRv= getProtocolSpec();
+		sRv+= getAddress();
+		return sRv;
+	}
+
+	string URL::getAddressQuery() const
+	{
+		string sRv(getAddress()), query;
+
+		query= getQuery();
+		if(query != "")
+			sRv+= "?" + query;
+		return sRv;
+	}
+
+	string URL::getBaseUri() const
+	{
+		string sRv(getSpecAddress()), query;
+
+		query= getQuery();
+		if(query != "")
+			sRv+= "?" + query;
+		return sRv;
+	}
+
+	void URL::encode()
+	{
+		encode(m_sHost, /*query*/false, /*anchor*/false);
+		encode(m_sPath, /*query*/false, /*anchor*/false);
+		encode(m_sFile, /*query*/false, /*anchor*/false);
+		for(vector<string>::iterator it= m_vsQuerys.begin(); it != m_vsQuerys.end(); ++it)
+			encode(*it, /*query*/true, /*anchor*/false);
+		encode(m_sAnchor, /*query*/true, /*anchor*/true);
+	}
+
+	void URL::encode(string& str, bool query, bool anchor)
+	{
+		char n;
+		vector<string> spl;
+		vector<string>::iterator it;
+		string::size_type c= 0;
+		ostringstream on;
+		string sNew;
+
+		if(	query &&
+			!anchor	)
+		{
+			// inside an query segment
+			// do not encode first is same sign '='
+			split(spl, str, is_any_of("="));
+			if(spl.size() > 1)
+			{
+				it= spl.begin();
+				str= *it;
+				++it;
+				sNew= *it;
+				++it;
+				while(it != spl.end())
+				{
+					sNew+= "=" + *it;
+					++it;
+				}
+				encode(str, query, /*anchor*/true);
+				encode(sNew, query, /*anchor*/true);
+				str+= "=" + sNew;
+				return;
+			}
+		}
+		while(c < str.length())
+		{
+			bool encode(false);
+
+			n= str.at(c);
+			// do not encode alphabetic characters
+			if(	n < '0' ||
+				(	n > '9' &&
+					n < 'A'		) ||
+				(	n > 'Z' &&
+					n < 'a'		) ||
+				n > 'z'				)
+			{
+				// do not encode [-], [_], [.] or [~]
+				if(	n != '-' &&
+					n != '_' &&
+					n != '.' &&
+					n != '~' 	)
+				{
+					// do not encode [/]
+					// when string is no query
+					if(	query ||
+						n != '/'	)
+					{
+						encode= true;
+					}
+				}
+			}
+			if(encode)
+			{
+				on.str("");
+				on << hex << (int)n;
+				sNew+= "%" + on.str();
+			}else
+				sNew+= n;
+			++c;
+		}
+		str= sNew;
+	}
+
+	void URL::decode()
+	{
+		decode(m_sHost);
+		decode(m_sPath);
+		decode(m_sFile);
+		for(vector<string>::iterator it= m_vsQuerys.begin(); it != m_vsQuerys.end(); ++it)
+			decode(*it);
+		decode(m_sAnchor);
+	}
+
+	void URL::decode(string& str)
+	{
+		string::size_type c= 0;
+		string sNew;
+
+		while(c < str.length())
+		{
+			if(str.at(c) == '%')
+			{
+				int n(0);
+				istringstream shex;
+
+				if(	(c + 2) < str.length())
+				{
+					string code(str.substr(c + 1, 2));
+					shex.str(code);
+					shex >> hex >> n;
+					if(!shex.fail())
+					{
+						sNew+= n;
+						c+= 2;
+					}else
+						sNew+= str.at(c);
+				}else
+					sNew+= str.at(c);
+			}else
+				sNew+= str.at(c);
+			++c;
+		}
+		str= sNew;
 	}
 }
--- server/src/util/URL.h
+++ server/src/util/URL.h
@@ -28,16 +28,63 @@
 #include "unistd.h"
 
 #include <string>
+#include <vector>
 #include <map>
 
 using namespace std;
 
 namespace util
 {
-
+	/**
+	 * enum of different URL types
+	 */
+	enum URLtype_e
+	{
+		unknown= 0,
+		file,
+		http,
+		https,
+		SOA
+	};
+	/**
+	 * object to hold any URL's
+	 * which can contain an normally file on hard disk,
+	 * or some internet address with different protocol's
+	 */
 	class URL
 	{
 		public:
+			/**
+			 * constructor to create empty object
+			 */
+			URL()
+			: m_eType(unknown),
+			  m_nPort(0)
+			{};
+			/**
+			 * constructor to create object with URL
+			 *
+			 * @param src URL for which object should created
+			 */
+			URL(string src)
+			: m_eType(unknown)
+			{ set(src); };
+			/**
+			 * write new URI into object.<br />
+			 * when URI is not from protocol type file,
+			 * it will be split address with query by first question mark '?',
+			 * the anchor by last grid '#'
+			 * and hole query by all ampersands '&'.<br />
+			 * when several query segments contain also an ampersand
+			 * or the anchor definition is not beginning by the last grid,
+			 * better define with this <code>set()</code> method
+			 * only the address
+			 * and all query segments with <code>addQuer()</code>.
+			 * Also the anchor with method <code>anchor()</code>.
+			 *
+			 * @param src new URL
+			 */
+			void set(const string& src);
 			/**
 			 * add first with second path,
 			 * only if the second not beginning
@@ -45,7 +92,7 @@ namespace util
 			 *
 			 * @param first first beginning path (working directory)
 			 * @param second second path or file
-			 * @param file boolean whether second is an file.<br />
+			 * @param always boolean whether second is an file.<br />
 			 * 				If parameter is true (DEFAULT),
 			 * 				the first path is only add when the second do not beginning
 			 * 				with an slash
@@ -61,19 +108,172 @@ namespace util
 			 */
 			static map<string, string> readDirectory(const string& path, const string& beginfilter, const string& endfilter);
 			/**
-			 * search user id in /etc/passwd
-			 *
-			 * @param user name of user
-			 * @return user id if found, elsewhere -1
-			 */
-			//static uid_t getUserID(const string& user);
-			/**
 			 * extract path from given path with file
 			 *
 			 * @param filestring file with path
 			 * @return only path
 			 */
 			static string getPath(const string& filestring);
+			/**
+			 * return protocol of URI
+			 *
+			 * @return URL type
+			 */
+			URLtype_e getProtocol() const
+			{ return m_eType; };
+			/**
+			 * return string of protocol specification
+			 *
+			 * @return specification
+			 */
+			string getProtocolSpec() const;
+			/**
+			 * return string of full address
+			 * (HOST/PATH/ and when protocol from file also with FILE)
+			 */
+			string getAddress() const;
+			/**
+			 * return string of full address of object
+			 * with specific URL protocol,
+			 * but without parameters
+			 *
+			 * @return address string
+			 */
+			string getSpecAddress() const;
+			/**
+			 * return full address of object,
+			 * without specific protocol type,
+			 * but with query when exist
+			 *
+			 * @return address string
+			 */
+			string getAddressQuery() const;
+			/**
+			 * return when protocol is no file type
+			 * an string of absolute path with
+			 * query string when exist
+			 *
+			 * @return path with query string
+			 */
+			string getAbsolutePathQuery() const;
+			/**
+			 * return full address of object
+			 * with specific protocol type
+			 * and also query when exist
+			 *
+			 * @return URI string
+			 */
+			string getBaseUri() const;
+			/**
+			 * return only host of address
+			 *
+			 * @return host string
+			 */
+			 string getHost() const
+			 { return m_sHost; };
+			 /**
+			  * return port from URL
+			  * when URL type is no file
+			  * otherwise 0
+			  *
+			  * @return port number
+			  */
+			 unsigned short getPort() const
+			 { return m_nPort; };
+			 /**
+			  * return path of address
+			  *
+			  * @return path string
+			  */
+			 string getAbsolutePath() const
+			 { return m_sPath; };
+			 /**
+			  * return file of address when exist
+			  *
+			  * @return file string
+			  */
+			 string getFile() const
+			 { return m_sFile; };
+			 /**
+			  * return an string of query
+			  * behind question mark when exist
+			  * otherwise an null string
+			  *
+			  * @return query string
+			  */
+			 string getQuery() const;
+			 /**
+			  * add one query segment
+			  * between ampersands '&'
+			  *
+			  * @param str query segment
+			  */
+			 void addQuery(const string& str)
+			 { m_vsQuerys.push_back(str); };
+			 /**
+			  * set anchor string
+			  * afer grid '#'
+			  *
+			  * @param str anchor string
+			  */
+			 void anchor(const string& str)
+			 { m_sAnchor= str; };
+			 /**
+			  * encode full URI
+			  */
+			 void encode();
+			 /**
+			  * decode full URI
+			  */
+			 void decode();
+
+
+		private:
+			/**
+			 * type of protocol or type file
+			 */
+			URLtype_e m_eType;
+			/**
+			 * host of internet address
+			 */
+			string m_sHost;
+			/**
+			 * port when URL is no file type
+			 */
+			unsigned short m_nPort;
+			/**
+			 * path of address
+			 */
+			string m_sPath;
+			/**
+			 * file of address
+			 */
+			string m_sFile;
+			/**
+			 * query parameters
+			 * after file when exist
+			 */
+			vector<string> m_vsQuerys;
+			/**
+			 * anchor parameter
+			 * after last grid sign '#'
+			 */
+			string m_sAnchor;
+
+			/**
+			 * encode given string
+			 *
+			 * @param str string to encode
+			 * @param query whether string is query of the URI
+			 * @param anchor wheter string is anchor of query
+			 */
+			void encode(string& str, bool query, bool anchor);
+			/**
+			 * decode given string
+			 *
+			 * @param str string to decode
+			 */
+			void decode(string& str);
 	};
 
 }
